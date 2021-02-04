6e1a56c18f57c71dd3f3268c3cdc9f8c66da70ef
Cox <Benjamin.Cox@dfw.wa.gov>
2021-02-04 13:40:26 -0800

    add dl_luts when dl_now true

--- R/get_release_data.R
+++ R/get_release_data.R
@@ -12,8 +12,10 @@ get_release_data <- function(first_by=NULL, last_by=NULL, rel_dir="RMIS",lut_dir
   #if(is.null(dir))dir <- "RMIS"
   rel_file <- file.path(rel_dir,"RL041_ALL_FULLSET.zip")
  
-  if((file.exists(rel_file) & dl_now==TRUE) | 
-     !file.exists(rel_file)) {download_releases(dir=rel_dir)}
+  if((file.exists(rel_file) & dl_now==TRUE) | !file.exists(rel_file)) {
+    download_releases(dir=rel_dir)
+    download_luts(lut_dir=lut_dir)
+    }
   
   read_releases(first_by=first_by, 
                 last_by=last_by,
