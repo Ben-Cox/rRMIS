3557e50cd18d02f495be8f636710e8ef031ba86e
Cox <Ben.Cox@dfw.wa.gov>
2024-03-22 16:17:03 -0700

    Fixed make_file_list to work with new rmpc pub data repo. code parses the html for links now instead of requesting paths from server.

--- R/make_file_list.R
+++ R/make_file_list.R
@@ -4,43 +4,31 @@
 #' @param endYear  Last year of desired CWT recovery data
 #' @param RMIS_url URL to RMIS ftp server
 #' @param temp_dir File to hold downloaded csv's
-#' @importFrom RCurl getURL
+#' @importFrom  purrr map_lgl 
+#' @importFrom XML htmlParse  xpathSApply
+#' @importFrom stringr str_detect str_extract
 #' @return A list with download urls for each file and destination paths for each file
 #'
 make_file_list <- function(startYear, 
                               endYear,
                               dir=NULL) {
-# # Error message
-#   if(is.null(startYear) | is.null(endYear)) {
 
-#     return(message("ERROR: Please supply start and/or end year"))
-#   }
+# Read the html of the public data url www.rmpc.org/pub/data
+doc <- XML::htmlParse(readLines(RMIS.globals$url), asText=TRUE)
+
+# Find links in the lines
+links <-XML::xpathSApply(doc, "//a/@href", simplify = T, unname)
+
+# Keep links to files starting with RC0 and ending with .csv
+csvs <- links[purrr::map_lgl(links, ~stringr::str_detect(.x,"^RC.*\\.csv$"))]
+
+# Extract 4-digit years for each recovery filename used to filter
+  yrs <- as.numeric(stringr::str_extract(csvs,"(?<=_)\\d{4}(?=\\.)"))
   
-  # load pkgs required for this function
-    #using("RCurl", "tidyverse")
-  
-# Get filenames in the RMIS ftp directory - Takes a minute
-ftp_names <- RCurl::getURL(RMIS.globals$url, dirlistonly = TRUE)
-
-# Make a vector of filenames, separating by line breaks ("\n")
-  filenames <- strsplit(ftp_names, "\n") [[1]]
-
-# Only want the csv files, use the regex below to grab only files beginning with RC041_ and ending with .csv
-    # Changed to .zip to help with download speed. readr can read from local zips but not url .zips
-  csvs <- filenames[grep("^RC042_.*\\.zip", filenames)] %>% 
-    lapply(function(x){regmatches(x, regexpr("href=\"\\s*\\K.*?(?=\\s*\")", x, perl=TRUE))}) %>% 
-  unlist()
-
-  #  Only grab ODFW/WDFW files, replace line 22 with:
-  #   csvs <-  filenames[grep("^RC041_ODFW.*\\.csv | ^RC041_WDFW.*\\.csv", filenames,fixed=F)]
-
-# Extract the years for each recovery filename,convert to number. used to filter out years we don't need
-  yrs <- substr(csvs, start=nchar(csvs)-7, nchar(csvs)-4) %>% as.integer
-
 # Subset the csv filenames for only the years we want, drop the "\r" from filenames (for when we go to download) 
-  files <- csvs[which(yrs >= startYear & yrs<=endYear)] %>% substr(1, nchar(.)-1)
+  files_to_download <- csvs[which(yrs >= startYear & yrs<=endYear)] #%>% substr(1, nchar(.)-1)
 
 # Return the list with a vector of file urls and the local path to save the downloaded files
-  list(download_urls=paste0(RMIS.globals$url, files), files=file.path(dir,files))
+  list(download_urls=paste0(RMIS.globals$url, files_to_download), files=file.path(dir,files_to_download))
 }
 
